cf1d4aebe2aa808613f754bf6ffe29957b0a2840
olyagoroshko <olga.og.goroshko@gmail.com>
2020-04-08 15:32:01 +0300

    Getting the remaining time for days

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor((t / 1000) % 60),
         minutes = Math.floor((t / 1000 / 60) % 60),
-        hours = Math.floor((t / (1000 * 60 * 60)));
-
+        //hours = Math.floor((t / (1000 * 60 * 60)));
+        hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
 
     return {
         'total': t,
